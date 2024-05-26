a4349c7fc680dd309038ed717348914a95d23884
Anatoly <sapphire.exe@gmail.com>
2024-05-27 01:36:09 +0300

    Finished homework 3 task 3

--- HW_3/hw_3_3.js
+++ HW_3/hw_3_3.js
@@ -1,4 +1,18 @@
 'use strict';
 
+/*
 Task3**. Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), и выводит сумму равную 
 n + nn + nnn, где n не перемножаются, а конкатенируются
+*/
+
+function concatProgram (n) {
+    let result;
+    if (n >= 1 && n <= 9 && String(n/1).length === 1) {
+        result = n + +(String(n) + n) + +(String(n) + n + n);
+    } else {
+        result = 'На входе должно быть целое положительное число n (одно любое число от 1 до 9).';
+    }
+    return result;
+}
+
+console.log(concatProgram(1.00));
